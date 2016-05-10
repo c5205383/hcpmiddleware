d8f7c67db9c88e02ebf0991d7a4ec031aee6464f
zerosrat <zero@zerosrat.com>
2016-05-10 16:23:26 +0800

    add several constant

--- src/main/java/com/hcp/objective/util/ODataConstants.java
+++ src/main/java/com/hcp/objective/util/ODataConstants.java
@@ -2,7 +2,11 @@ package com.hcp.objective.util;
 
 public class ODataConstants {
 	public static final String HTTP_METHOD_GET = "GET";
+	public static final String HTTP_METHOD_POST = "POST";
 	public static final String APPLICATION_ATOM_XML = "application/atom+xml";
 	public static final String SEPARATOR = "/";
 	public static final String DATE_FROMAT = "yyyy-MM-dd";
+	public static final String HTTP_HEADER_ACCEPT= "Accept";
+	public static final String HTTP_HEADER_CONTENT_TYPE = "Content-Type";
+	public static final String APPLICATION_JSON = "application/json";
 }

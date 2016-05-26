c6eb260b02b16cf79678d2f2c61f05ad8973e58b
zerosrat <zero@zerosrat.com>
2016-05-26 13:58:56 +0800

    use IOUtils to get data

--- src/main/java/com/hcp/objective/web/HomeController.java
+++ src/main/java/com/hcp/objective/web/HomeController.java
@@ -109,24 +109,27 @@ public class HomeController {
 			String authorizationHeader = authType + " ";
 			authorizationHeader += new String(Base64.encodeBase64((auth).getBytes()));
 
-			StringBuilder result = new StringBuilder();
+			//StringBuilder result = new StringBuilder();
 			String absolutUri = odataExecutor.createUri(serviceUrl, entitySetName, eid, expand, query);
 			URL url = new URL(absolutUri);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Authorization", authorizationHeader);
 
-			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String line;
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-			}
-
+//			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//			String line;
+//			while ((line = rd.readLine()) != null) {
+//				result.append(line);
+//			}
+			InputStream in = conn.getInputStream();
+			String encoding = conn.getContentEncoding();
+			encoding = encoding == null ? "UTF-8" : encoding;
+			String body = IOUtils.toString(in, encoding);
 			conn.disconnect();
+			in.close();
+			//rd.close();
 
-			rd.close();
-
-			return result.toString();
+			return body;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
